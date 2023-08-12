d8b711394aa3c2a991fba4dc022e59ec2db2d00a
Bhavya Joshi <bjoshi1503@gmail.com>
2023-08-12 13:09:15 +0530

    linked lists

---
+++ src/LinkedLists/Main.java
@@ -0,0 +1,14 @@
+package LinkedLists;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(78);
+        list.insertFirst(6);
+        list.insertFirst(37);
+        list.display();
+
+    }
+}
